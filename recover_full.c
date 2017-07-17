6381ea3081bd9ac27e664bbbeab832bb0e0995e6
Harpreet Sangar <sangarhappy@gmail.com>
2017-07-17 23:02:50 +0530

    Update recover_full.c

--- recover_full.c
+++ recover_full.c
@@ -46,26 +46,26 @@ int main(int argc, char** argv)
 	}
 
 	char name[7];
-	int i = 0, check = 0;
-	FILE* image;
+	int i = 0, k;
+	FILE* image = NULL;
 	
-	while( (feof(card)) == 0 )
+	while( feof(card) == 0 )
 	{
 		fread(buffer, sizeof(BYTE), 512, card);
+
 		
-		for(int k = 0; k < 512; k++)
+		for(k = 0; k < 512; k++)
 		{
-			if(buffer[k] == 0xff &&
-	  			buffer[k + 1] == 0xd8 &&
+			if(buffer[k] != 0xff)
+				continue;
+
+			if(	buffer[k + 1] == 0xd8 &&
 	  			buffer[k + 2] == 0xff &&
 	  			(buffer[k + 3] & 0xf0) == 0xe0)
 			{
 				// if outfile already opened, close it
-				if(check == 1)
-				{	
+				if(image != NULL)
 					fclose(image);
-					check = 0;
-				}
 				
 				// open new outfile
 				sprintf(name, "%03d.jpg", i);
@@ -79,10 +79,9 @@ int main(int argc, char** argv)
 						fprintf(stderr, "Could not create image\n");
 						return 4;
 					}
-				check = 1;
 			}
 				
-			if(check == 1)
+			if(image != NULL)
 			{	
 				for(int j = k; j < 512; j++)
 					fwrite(&buffer[j], sizeof(BYTE), 1, image);
@@ -93,7 +92,7 @@ int main(int argc, char** argv)
 	
 	fclose(card);
 	free(buffer);
-	if(check == 1)
+	if(image != NULL)
 		fclose(image);
 	return 0;
 }
